178fb002d8cf254e0040f6c8407bdc8bf7fb82ba
d00476471 <d00476471@utahtech.edu>
2025-11-20 16:08:57 -0700

    Added google maps embed and github links for project

--- milestone6/quicksort.py
+++ milestone6/quicksort.py
@@ -12,7 +12,7 @@ async def bubbleSort(A):
     display(A, target="quicksort-output", append=False)
 
 def main():
-    list1 = [3,5,1,6,5,2,9,0,4,6]
+    list1 = [3,5,1,6,5,2,9,0,4,6,1,2,5,4,6]
     asyncio.ensure_future(bubbleSort(list1))
     
 main()
