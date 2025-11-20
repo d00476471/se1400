6a04d0398a74e162435271bf7349487d795d0ab4
d00476471 <d00476471@utahtech.edu>
2025-11-19 17:38:04 -0700

    bubblesort now shows progress

--- milestone6/quicksort.py
+++ milestone6/quicksort.py
@@ -1,16 +1,18 @@
 from pyscript import display
-import time
+import asyncio
 
-def bubbleSort(A):
+async def bubbleSort(A):
     F = len(A)
     for i in range(F):
         for j in range(0,F-i-1):
             if A[j] > A[j+1]:
                 A[j], A[j+1] = A[j+1], A[j]
+                display(A, target="quicksort-output", append=False)
+                await asyncio.sleep(0.5)
+    display(A, target="quicksort-output", append=False)
 
 def main():
     list1 = [3,5,1,6,5,2,9,0,4,6]
-    bubbleSort(list1)
-    display(list1, target="quicksort-output", append=True)
-
+    asyncio.ensure_future(bubbleSort(list1))
+    
 main()
